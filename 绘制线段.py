99a7e60eba8b3590f722ab6bb590184854bf1372
TiMeMof <103636463+TiMeMof@users.noreply.github.com>
2022-05-07 16:48:29 +0800

    Update 绘制线段.py

--- 绘制线段.py
+++ 绘制线段.py
@@ -1,10 +1,14 @@
 import numpy as np
 import cv2
 
-canvas = np.ones((300, 300, 3), np.uint8)*111
+canvas = np.ones((300, 300, 3), np.uint8)*255
 
-canvas = cv2.line(canvas, (50, 50), (250, 250), (111, 222, 333), 3)
-
+canvas = cv2.line(canvas, (50, 50), (250, 250), (1, 222, 3), 3)
+canvas = cv2.line(canvas, (250, 50), (50, 250), (1, 222, 3), 3)
+canvas = cv2.line(canvas, (50, 50), (250, 50), (111, 2, 3), 3)
+canvas = cv2.line(canvas, (250, 50), (250, 250), (111, 2, 3), 3)
+canvas = cv2.line(canvas, (250, 250), (50, 250), (111, 2, 3), 3)
+canvas = cv2.line(canvas, (50, 250), (50, 50), (111, 2, 3), 3)
 cv2.imshow("lines", canvas)
 cv2.waitKey()
 cv2.destroyAllWindows()
